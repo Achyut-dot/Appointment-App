d9f7da09cbdeb266fa875f16b0c4011dd6394932
Achyut Raghuvanshi <achyut24122002@gmail.com>
2025-03-02 20:28:22 +0530

    Added Appointment in Array using Two Way Data Binging

--- src/app/appointment-list/appointment-list.component.ts
+++ src/app/appointment-list/appointment-list.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { Appointment } from '../models/appointment';
 
-
 @Component({
   selector: 'app-appointment-list',
   templateUrl: './appointment-list.component.html',
@@ -12,9 +11,21 @@ export class AppointmentListComponent {
   newAppointmentTitle: string = "";
   newAppointmentDate: Date = new Date();
 
-  appointment: Appointment[] = []
+  appointments: Appointment[] = []
 
   addAppointement() {
-    alert(this.newAppointmentTitle + " " + this.newAppointmentDate)
+    if (this.newAppointmentTitle.trim().length && this.newAppointmentDate) {
+      let newAppointment: Appointment = {
+        id: Date.now(),
+        title: this.newAppointmentTitle,
+        date: this.newAppointmentDate
+      }
+      this.appointments.push(newAppointment)
+
+      this.newAppointmentTitle = "";
+      this.newAppointmentDate = new Date();
+
+      alert(this.appointments.length)
+    }
   }
 }
